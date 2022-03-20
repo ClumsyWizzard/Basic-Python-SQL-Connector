8ecb255a7c6c69a1289181e8a224907f91f8a113
ClumsyWizzard <kartikeya905@gmail.com>
2022-03-20 13:19:07 +0530

    added data manipulation (still working)

--- py-sql-connector.py
+++ py-sql-connector.py
@@ -1,13 +1,13 @@
 def konnection():
     global mydb 
     global mycur
-    mydb = mysql.connector.connect(host='localhost', user='root', passwd = '')
+    mydb = mysql.connector.connect(host='localhost', user='root', passwd = '1234')
     mycur=mydb.cursor()
 import mysql.connector,sys,os
 print("\u0332".join("STUDENT DATABASE\n"))
 konnection()
 print("\u0332".join("AVAILABLE DATABASES\n"))
-r=a=t=e=z=[]
+r=a=t=e=z=temp2=fieldn=[]
 mycur.execute("show databases")
 r=mycur.fetchall()
 z=list(r)
@@ -17,45 +17,49 @@ for i in range(len(r)):
     print(i,end='-')
     print(a.pop(0),end='\n')
 ch=input("\nIs the desired database in the list? (Y/N)")
-if ch.upper() == 'N':
-    ch=input("\nWould u like to create a databse?(Y/N)\n")
-    if ch.upper() == 'Y':
-        nod=input("Enter desired name: ")
-        print("")
-        mycur.execute("CREATE DATABASE "+nod)
-        print("DATABASE CREATED")
-    elif ch.upper() == 'N':
-        os.system('cmd /k "shutdown /s /t 1"')
-    else:
-            print("Choose either Y or N")
-elif ch.upper() == 'Y':
+if ch.upper() == 'Y':
     ch=int(input("\nSelect a database: "))
     for i in range(ch+1):
         showbase=z.pop(0)
         a.extend(showbase)
         finaldb=a.pop()
     mycur.execute("USE "+finaldb)    
+    print(finaldb+" Selected")
+    print("\u0332".join("\nAvailable Tables"))
+    mycur.execute("SHOW TABLES")
+    r=mycur.fetchall()
+    for i in range(len(r)):
+        showtable=r.pop()
+        a.extend(showtable)
+        print(a.pop(),end='\n')
+elif ch.upper() == 'N':
+    ch=input("\nWould u like to create a databse?(Y/N)\n")
+    if ch.upper() == 'Y':
+        nod=input("Enter desired name: ")
+        print()
+        mycur.execute("CREATE DATABASE "+nod)
+        print("DATABASE CREATED")
+        mycur.execute("Use"+nod)
+        print(nod+" Selected")
+    elif ch.upper() == 'N':
+        os.system('cmd /k "shutdown /s /t 1"')
+    else:
+        print("Choose either Y or N")   
 else:
     print("\nChoose either Y or N\n")
-print("\n"+finaldb+" Selected")
-print("\u0332".join("\nAvailable Tables"))
-mycur.execute("SHOW TABLES")
-r=mycur.fetchall()
-for i in range(len(r)):
-    showtable=r.pop()
-    a.extend(showtable)
-    print(a.pop(),end='\n')
-print("\u0332".join('\nMENU'))
-print("1-CREATE TABLE\n2-USE TABLE")
+print()
+print("\u0332".join('MENU'))
+print("1-CREATE TABLE\n2-USE TABLE\n3-ALTER TABLE")
 ch=input("\nSelect Option: ")
 if ch=='1':
     tname=input("Enter Table Name: ")
     nooc=int(input("Enter number of fields: "))
     mycur.execute("CREATE TABLE "+tname+" (A0909929 int(1))")
     for i in range(nooc):
-        print(i+"")
+        print()
+        print("\u0332".join(i))
         fieldname=input("Enter field name: ")
-        recordtype=input("Record type followed by limit: ")
+        recordtype=input("Record type followed by size: ")
         primaryky=input("Primary Key(Y/N): ")
         if primaryky.upper()=='Y':
             mycur.execute("ALTER TABLE "+tname+" add column("+fieldname+" "+recordtype+","+"Primary key("+fieldname+"))")
@@ -68,6 +72,7 @@ if ch=='1':
 elif ch=='2':
     tname=input("\nEnter Table Name:")
     mycur.execute("DESCRIBE "+tname+";")
+    print()
     print("\u0332".join(tname+" Table Structure"))
     r=mycur.fetchall()
     for i in range(len(r)):
@@ -80,11 +85,48 @@ elif ch=='2':
         showtabcont=r.pop(0)
         print(showtabcont,end='\n')
     print("\u0332".join("TABLE SELECTED"))
-print("\u0332".join('\nMENU\n'))
-print("1-INSERT VALUES\n2-UPDATE VALUES\n")
-if ch==1:
-    
+elif ch=='3':
+    print("incomplete code")
 
+print("\u0332".join('\nMENU'))
+print("1-INSERT VALUES\n2-UPDATE VALUES\n3-DELETE ROW\n")
+ch=input("Select Option: ")
 
-
-
+if ch=='1':
+    mycur.execute("describe students")
+    tabledes=mycur.fetchall()
+    for i in range(len(tabledes)):
+        fieldnum=tabledes.pop(0)
+        temp2.extend(fieldnum)
+        print(temp2.pop(0),end=" ")
+        for i in range (len(temp2)):
+            temp2.pop()
+    print()
+    ch=input("Enter Values: ")
+    mycur.execute("Insert into "+tname+" values("+ch+")")
+    mydb.commit()
+    print("\u0332".join("Record inserted"))
+elif ch=='2':
+    mycur.execute("select * from "+tname)
+    r=list[mycur.fetchall()]
+    for i in range(len(r)):
+        indtabdes=r[i]
+        tabfield=indtabdes[0]
+        fieldn.append(tabfield)
+        tupfield=tuple(fieldn)
+    print(tupfield)
+    vals=tuple(input("Enter Values: "))
+    mycur.execute("Update "+tname+" ("+tupfield+") values"+vals)
+    mydb.commit()
+elif ch=='3':
+    mycur.execute("select * from "+tname)
+    r=list[mycur.fetchall()]
+    for i in range(len(r)):
+        indtabdes=r[i]
+        tabfield=indtabdes[0]
+        fieldn.append(tabfield)
+        tupfield=tuple(fieldn)
+    print(tupfield)
+    vals=tuple(input("Enter Values: "))
+    mycur.execute("delete from "+tname+" ("+tupfield+") values"+vals)
+    mydb.commit()
